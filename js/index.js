d3d4aec5434cea6a76a0889d424d8c9e36c42ab0
Mesabloo <22964017+Mesabloo@users.noreply.github.com>
2021-07-01 20:56:28 +0200

    set tab bar color according to active tab

--- js/index.js
+++ js/index.js
@@ -4,6 +4,34 @@ let flask
 let split
 
 document.addEventListener('DOMContentLoaded', e => {
+  let tabbar = document.querySelector('#tabs')
+
+  document.querySelector('#editor-view').addEventListener('click', e => {
+    if (!tabbar.classList.contains('bg-purple')) {
+      // TODO:
+      // - Set tab to #editor-tab
+      tabbar.classList.toggle('bg-purple')
+      tabbar.classList.toggle('bg-dark-gray')
+      // - Change tab bar color (toggle `bg-purple` and `bg-dark-gray`)
+    }
+  })
+  document.querySelector('#compiler-view').addEventListener('click', e => {
+    if (!tabbar.classList.contains('bg-dark-gray')) {
+      // TODO:
+      // - Set tab to #debug-tab
+      tabbar.classList.toggle('bg-purple')
+      tabbar.classList.toggle('bg-dark-gray')
+      // - Change tab bar color (toggle `bg-purple` and `bg-dark-gray`)
+    }
+  })
+  document.querySelector('#run').addEventListener('click', e => {
+    // TODO:
+    // - Change icon to fa-stop-circle
+    // - Get code and send a request to the backend
+    // - Wait for request to end (correctly or timeout)
+    // - Change icon back to fa-play
+  })
+
 //  split = new Split(['#code-editor-pane', '#debug-pane'], {
 //    sizes: [75, 25],
 //    minSizes: [200, 100],
