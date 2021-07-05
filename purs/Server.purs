8bc6128f2b5005ed564da55d02c9019345bc55c7
Mesabloo <22964017+Mesabloo@users.noreply.github.com>
2021-07-05 21:25:30 +0200

    setup the output directory the server is ok

--- purs/Server.purs
+++ purs/Server.purs
@@ -1,12 +1,20 @@
 module Server (runServer) where
 
 import CLI (CLI)
+import Effect.Aff (launchAff_)
 import Effect.Console as Console
 import HTTPure as HTTPure
+import Node.FS.Aff as FS
 import Prelude
 import Server.Router (serverRouter)
 
 -- | Runs the server on the given port.
 runServer :: CLI -> HTTPure.ServerM
-runServer { port } = HTTPure.serve port serverRouter do
-  Console.log $ "Server running on port " <> show port <> "."
+runServer { port } =
+  let outDir = "./out"
+  in HTTPure.serve port (serverRouter outDir) do
+    launchAff_ do
+      outExists <- FS.exists outDir
+      unless outExists do
+        FS.mkdir outDir
+    Console.log $ "Server running on port " <> show port <> "."
